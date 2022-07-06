2d6c0fd73c3794da9a0d7b1e2dffbe22164e1382
Dale Short <short.dale@gmail.com>
2022-07-06 18:54:28 -0400

    style home page and add border demo

--- latin_squares/src/App.js
+++ latin_squares/src/App.js
@@ -4,11 +4,46 @@ import Square from "./Square";
 
 function App() {
   return (
-    <div className="App">
-      Some text
-      <div>
-        <Square />
-      </div>
+    <div class="App">
+      <nav class="navbar">
+        <div class="container">
+          <div class="logo">Latin Squares</div>
+          <ul class="nav">
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <header class="header">
+        <div class="container">
+          <div>
+            <h1>Latin Square Sudoku</h1>
+            <p>A crazy game of Sudoku that I barely understand.</p>
+          </div>
+        </div>
+        <div class="container" id="app">
+          <Square />
+        </div>
+      </header>
+
+      <section class="boxes">
+        <div class="container">
+          <div class="box">
+            <h2>
+              <i class="fas fa-mobile"></i>How to play
+            </h2>
+            <p>This could be some instructions about how to play the game!</p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
--- latin_squares/src/Square.js
+++ latin_squares/src/Square.js
@@ -4,10 +4,14 @@ import "./Square.css";
 class Square extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: "" };
+    this.state = { value: "", lastValue: "", my_style: "" };
 
     this.handleChange = this.handleChange.bind(this);
     //  this.handleSubmit = this.handleSubmit.bind(this);
+    //do we need this bind still?  What does it do?
+  }
+  componentDidMount() {
+    this.setState({ my_style: "outer-box double-top" });
   }
 
   handleChange(event) {
@@ -16,12 +20,29 @@ class Square extends React.Component {
     //its not valid clear the value
     //(do we need to force the text in the text box to clear?)
     let text_received;
-    text_received = event.target.value.charAt(event.target.value.length - 1);
+
+    console.log("event text received" + event.target.value);
+    if (isNaN(event.target.value)) {
+      text_received = event.target.value;
+    } else if (event.target.value.length >= 2) {
+      // text is "12" lastvalue is 1  -> we want 2  indexOf(last) = 0
+      // text is "12" lastvalue is 2 -> we want 1   indexOf(lastValue) = 1
+      // text is "21" lastvalue is 2 -> we want 1
+      // text is '21' lastvalue is 1 -> we want 2
+
+      if (event.target.value.indexOf(this.state.lastValue) == 0) {
+        text_received = event.target.value.charAt(1);
+      } else {
+        text_received = event.target.value.charAt(0);
+      }
+      this.setState({ lastValue: text_received });
+    } else {
+      text_received = event.target.value;
+    }
 
     if (["1", "2", "3", "4", "5", "6", "7", "8", "9"].includes(text_received)) {
       this.setState({ value: text_received });
     } else if (text_received === "\n" || text_received === "\r") {
-      console.log(text_received);
     } else {
       this.setState({ value: "" });
       //clear the value in the textbook
@@ -39,7 +60,7 @@ class Square extends React.Component {
 
   render() {
     return (
-      <div>
+      <div class={this.state.my_style}>
         <form onSubmit={this.handleSubmit}>
           <label>
             <input
@@ -53,7 +74,6 @@ class Square extends React.Component {
             />
           </label>
         </form>
-        <div>our state of value is {this.state.value}</div>
       </div>
     );
   }
