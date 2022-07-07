dbbfae6667068d2bab7c8c5f45008fbd8415729e
Dale Short <short.dale@gmail.com>
2022-07-07 12:45:43 -0400

    add grid generation Co-authored-by: James Monroe Hammer <11713717+jmh0036@users.noreply.github.com>"

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,32 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className="container">
-          <div class="item1 box-1"></div>
-          <div class="box-2">1</div>
-          <div class="box-2">2</div>
-          <div class="box-1">3</div>
-          <div class="box-1">4</div>
-          <div class="box-1">5</div>
-          <div class="box-1">6</div>
-          <div class="box-1">7</div>
-          <div class="box-1">8</div>
-          <div class="box-1">9</div>
-          <div class="box-1">10</div>
-          <div class="box-1">11</div>
-          <div class="box-1">12</div>
-          <div class="box-1">13</div>
-          <div class="box-1">14</div>
-          <div class="box-1">15</div>
-          <div class="box-1">16</div>
-        </div>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    var my_array = Array.from({ length: 16 }, (x, i) => i);
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="container">
+            {my_array.map(function (count) {
+              return <div className="box-1">{count}</div>;
+            })}
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
