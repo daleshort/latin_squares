2b62f941984089d308bdae49e2da273fc72ea966
jmh0036 <jmh0036@tigermail.auburn.edu>
2022-07-07 10:50:50 -0400

    Attempt at Grid on Grid Action

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,8 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="container">
-          <div class="box-1">1</div>
-          <div class="box-1">2</div>
+          <div class="item1 box-1"></div>
+          <div class="box-2">1</div>
+          <div class="box-2">2</div>
           <div class="box-1">3</div>
           <div class="box-1">4</div>
           <div class="box-1">5</div>
@@ -15,6 +16,13 @@ function App() {
           <div class="box-1">7</div>
           <div class="box-1">8</div>
           <div class="box-1">9</div>
+          <div class="box-1">10</div>
+          <div class="box-1">11</div>
+          <div class="box-1">12</div>
+          <div class="box-1">13</div>
+          <div class="box-1">14</div>
+          <div class="box-1">15</div>
+          <div class="box-1">16</div>
         </div>
       </header>
     </div>
