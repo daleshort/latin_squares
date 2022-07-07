84718f2e06148024df3bfc8f803b72f2d15aeca0
Dale Short <short.dale@gmail.com>
2022-07-07 09:12:39 -0400

    adding grid test

---
+++ my-app/src/App.js
@@ -0,0 +1,24 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="container">
+          <div class="box-1">1</div>
+          <div class="box-1">2</div>
+          <div class="box-1">3</div>
+          <div class="box-1">4</div>
+          <div class="box-1">5</div>
+          <div class="box-1">6</div>
+          <div class="box-1">7</div>
+          <div class="box-1">8</div>
+          <div class="box-1">9</div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
