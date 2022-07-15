25a234ffcb74f68638accd26a8b4cd3aa13834d0
Dale Short <short.dale@gmail.com>
2022-07-15 13:53:59 -0400

    add pipenv and gunicorn server

--- backend/sudoku/api/app.py
+++ backend/sudoku/api/app.py
@@ -11,6 +11,7 @@ from typing import List, Tuple
 # current module (__name__) as argument.
 app = Flask(__name__)
 
+
 def solve_sudoku(sizes, grid):
     N = len(grid)
     # """ An efficient Sudoku solver using Algorithm X.
@@ -21,9 +22,10 @@ def solve_sudoku(sizes, grid):
     for size in sizes:
         R, C = size
         # N = R * C
-        X += [("r%s"%str(size), rn) for rn in product(range(N), range(1, N + 1))]
+        X += [("r%s" % str(size), rn)
+              for rn in product(range(N), range(1, N + 1))]
         for r, c, n in product(range(N), range(N), range(1, N + 1)):
-            Y[(r, c, n)] += [("r%s"%str(size), ((r // R) * R + (c // C), n))]
+            Y[(r, c, n)] += [("r%s" % str(size), ((r // R) * R + (c // C), n))]
     X, Y = exact_cover(X, Y)
     for i, row in enumerate(grid):
         for j, n in enumerate(row):
@@ -34,12 +36,14 @@ def solve_sudoku(sizes, grid):
             grid[r][c] = n
         yield grid
 
+
 def exact_cover(X, Y):
     X = {j: set() for j in X}
     for i, row in Y.items():
         for j in row:
             X[j].add(i)
     return X, Y
+
 
 def solve(X, Y, solution):
     if not X:
@@ -54,6 +58,7 @@ def solve(X, Y, solution):
             deselect(X, Y, r, cols)
             solution.pop()
 
+
 def select(X, Y, r):
     cols = []
     for j in Y[r]:
@@ -64,6 +69,7 @@ def select(X, Y, r):
         cols.append(X.pop(j))
     return cols
 
+
 def deselect(X, Y, r, cols):
     for j in reversed(Y[r]):
         X[j] = cols.pop()
@@ -71,6 +77,7 @@ def deselect(X, Y, r, cols):
             for k in Y[i]:
                 if k != j:
                     X[k].add(i)
+
 
 class Cell:
     def __init__(self, value: str, id: int, row: int, col: int, value_start: vars, value_correct: int) -> None:
@@ -82,14 +89,18 @@ class Cell:
         self.value_correct = value_correct
 
 # object with keyworks (row, column, Rectangular Regions)
+
+
 class Board:
-    def __init__(self, row: Tuple[int,int], col: Tuple[int,int], regions: List[Tuple[int, int]]) -> None:
+    def __init__(self, row: Tuple[int, int], col: Tuple[int, int], regions: List[Tuple[int, int]]) -> None:
         self.row = row
         self.col = col
         self.regions = regions
 
 # ‘/’ URL is bound with MakePuzzle() function.
-def MakePuzzle(WhatIsLatin:Board = Board((1,6),(6,1),[(2,3),(3,2)])) -> str:
+
+
+def MakePuzzle(WhatIsLatin: Board = Board((1, 6), (6, 1), [(2, 3), (3, 2)])) -> str:
     # Let's define the list of things to be latin so we can reuse it.
     LatinList = [WhatIsLatin.row, WhatIsLatin.col]
     for i in WhatIsLatin.regions:
@@ -99,7 +110,7 @@ def MakePuzzle(WhatIsLatin:Board = Board((1,6),(6,1),[(2,3),(3,2)])) -> str:
     Order = LatinList[0][0]*LatinList[0][1]
 
     # Creating a random first row
-    RemainigChoices = list(range(1,Order+1))
+    RemainigChoices = list(range(1, Order+1))
     FirstRow = []
     while RemainigChoices != []:
         NextElement = rnd.choice(RemainigChoices)
@@ -114,16 +125,17 @@ def MakePuzzle(WhatIsLatin:Board = Board((1,6),(6,1),[(2,3),(3,2)])) -> str:
     UniquePuzzle = []
 
     # Pick a solution to trim from.
-    WhichSol = rnd.randint(0,1935)
-    TestPuzzle = solution = list(islice(solve_sudoku(LatinList,TestPuzzle),WhichSol,WhichSol+1))[0]
+    WhichSol = rnd.randint(0, 1935)
+    TestPuzzle = solution = list(
+        islice(solve_sudoku(LatinList, TestPuzzle), WhichSol, WhichSol+1))[0]
     numOfSolutions = 1
     while numOfSolutions == 1:
         UniquePuzzle = copy.deepcopy(TestPuzzle)
-        randomRow = rnd.randint(0,Order-1)
-        randomCol = rnd.randint(0,Order-1)
+        randomRow = rnd.randint(0, Order-1)
+        randomCol = rnd.randint(0, Order-1)
         TestPuzzle[randomRow][randomCol] = 0
-        solution = solve_sudoku(LatinList,copy.deepcopy(TestPuzzle))
-        numOfSolutions = 0 
+        solution = solve_sudoku(LatinList, copy.deepcopy(TestPuzzle))
+        numOfSolutions = 0
         for sol in solution:
             numOfSolutions += 1
             if numOfSolutions > 1:
@@ -135,14 +147,15 @@ def MakePuzzle(WhatIsLatin:Board = Board((1,6),(6,1),[(2,3),(3,2)])) -> str:
         for col in range(len(TestPuzzle)):
             TestPuzzle[row][col] = 0
             numOfSolutions = 0
-            for sol in solve_sudoku(LatinList,copy.deepcopy(TestPuzzle)):
+            for sol in solve_sudoku(LatinList, copy.deepcopy(TestPuzzle)):
                 numOfSolutions += 1
             if numOfSolutions == 1:
                 UniquePuzzle = copy.deepcopy(TestPuzzle)
             else:
                 TestPuzzle = copy.deepcopy(UniquePuzzle)
 
-    CorrectAnswer = list(solve_sudoku(LatinList,copy.deepcopy(UniquePuzzle)))[0]
+    CorrectAnswer = list(solve_sudoku(
+        LatinList, copy.deepcopy(UniquePuzzle)))[0]
 
     # Construct List of cell objects
     ListOfCells = []
@@ -150,7 +163,8 @@ def MakePuzzle(WhatIsLatin:Board = Board((1,6),(6,1),[(2,3),(3,2)])) -> str:
     for i in range(len(UniquePuzzle)):
         for j in range(len(UniquePuzzle)):
             idno += 1
-            ListOfCells.append(Cell('null',idno,i,j,'null' if UniquePuzzle[i][j] == 0 else UniquePuzzle[i][j],CorrectAnswer[i][j]))
+            ListOfCells.append(Cell(
+                'null', idno, i, j, 'null' if UniquePuzzle[i][j] == 0 else UniquePuzzle[i][j], CorrectAnswer[i][j]))
 
     # Write to JSON file
     jsonStr = json.dumps([ob.__dict__ for ob in ListOfCells])
@@ -160,21 +174,26 @@ def MakePuzzle(WhatIsLatin:Board = Board((1,6),(6,1),[(2,3),(3,2)])) -> str:
 # The route() function of the Flask class is a decorator,
 # which tells the application which URL should call
 # the associated function.
+
+
 @app.route('/GetPuzzle')
 def MakeOrder6():
-    return MakePuzzle(Board((1,6),(6,1),[(2,3),(3,2)]))
+    return MakePuzzle(Board((1, 6), (6, 1), [(2, 3), (3, 2)]))
+
 
 @app.route('/GetPuzzle8')
 def MakeOrder8():
-    return MakePuzzle(Board((1,8),(8,1),[(2,4),(4,2)]))
+    return MakePuzzle(Board((1, 8), (8, 1), [(2, 4), (4, 2)]))
+
 
 @app.route('/GetPuzzle10')
 def MakeOrder10():
-    return MakePuzzle(Board((1,10),(10,1),[(2,5),(5,2)]))
+    return MakePuzzle(Board((1, 10), (10, 1), [(2, 5), (5, 2)]))
+
 
 # main driver function
 if __name__ == '__main__':
-	# run() method of Flask class runs the application
-	# on the local development server.
+    # run() method of Flask class runs the application
+    # on the local development server.
     # app = MakePuzzle(Parts)
     app.run()
