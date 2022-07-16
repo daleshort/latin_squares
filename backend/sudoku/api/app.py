aec6e3a1b632611aae38bbb3255e10fc22e3f565
James Hammer <james.hammer3@gmail.com>
2022-07-16 12:21:17 -0400

    Changed 'null' to None

--- backend/sudoku/api/app.py
+++ backend/sudoku/api/app.py
@@ -150,7 +150,7 @@ def MakePuzzle(WhatIsLatin:Board = Board((1,6),(6,1),[(2,3),(3,2)])) -> str:
     for i in range(len(UniquePuzzle)):
         for j in range(len(UniquePuzzle)):
             idno += 1
-            ListOfCells.append(Cell('null',idno,i,j,'null' if UniquePuzzle[i][j] == 0 else UniquePuzzle[i][j],CorrectAnswer[i][j]))
+            ListOfCells.append(Cell(None,idno,i,j,None if UniquePuzzle[i][j] == 0 else UniquePuzzle[i][j],CorrectAnswer[i][j]))
 
     # Write to JSON file
     jsonStr = json.dumps([ob.__dict__ for ob in ListOfCells])
