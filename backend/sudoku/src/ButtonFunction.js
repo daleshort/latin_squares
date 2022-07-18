aeb011589e13addd5facd2b969dd12002e54a70f
James Hammer <james.hammer3@gmail.com>
2022-07-18 17:32:39 -0400

    Working local Version

---
+++ backend/sudoku/src/ButtonFunction.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from "react";
+
+function ButtonFunction({
+  style,
+  className,
+  my_key,
+  my_row,
+  my_col,
+  isSelected,
+  isInHighlight,
+  onClickFunction,
+  value,
+  handleFocusAwayFunction,
+  value_correct,
+  value_start,
+  showSolution,
+  loadCount,
+}) {
+
+  return (
+
+  );
+}
+
+export default ButtonFunction;
